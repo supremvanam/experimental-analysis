34457b85d1116cc6a56c030a1104fede9c67c047
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-06-14 00:18:28 +0530

    Updated Line Graph

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,30 +2,22 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
-
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
-    @FXML
-    private CategoryAxis x;
-
-    @FXML
-    private CategoryAxis y;
 
     @FXML
     LineChart<String, Number> lineChart;
 
-    XYChart.Series algorithmOne = new XYChart.Series();
-    XYChart.Series algorithmTwo = new XYChart.Series();
+    XYChart.Series<String, Number> algorithmOne = new XYChart.Series<>();
+    XYChart.Series<String, Number> algorithmTwo = new XYChart.Series<>();
 
 
-
-    public static void experimentalAnalysis1(int n, int trials, XYChart.Series series) {
+    public static void experimentalAnalysis1(int n, int trials, XYChart.Series<String, Number> series) {
         series.setName("Algorithm 1");
         System.out.println("<----------- Prefix Average 1 ----------->");
         for (int i = 0; i<trials; i++) {
@@ -39,7 +31,7 @@ public class Controller implements Initializable {
         }
     }
 
-    public static void experimentalAnalysis2(int n, int trials, XYChart.Series series) {
+    public static void experimentalAnalysis2(int n, int trials, XYChart.Series<String, Number> series) {
         series.setName("Algorithm 2");
         System.out.println("<----------- Prefix Average 2 ----------->");
         for (int i = 0; i<trials; i++) {
@@ -56,26 +48,13 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
-        experimentalAnalysis1(1000, 5, algorithmOne);
-        experimentalAnalysis2(1000, 5, algorithmTwo);
+        experimentalAnalysis1(1000, 7, algorithmOne);
+        experimentalAnalysis2(1000, 10, algorithmTwo);
 
         lineChart.getData().addAll(algorithmOne);
         lineChart.getData().addAll(algorithmTwo);
 
     }
-
-
-//        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
-//        series.getData().add(new XYChart.Data<>("Jan", 200));
-//        series.getData().add(new XYChart.Data<>("Feb", 275));
-//        series.getData().add(new XYChart.Data<>("Mar", 120));
-//        series.getData().add(new XYChart.Data<>("Apr", 310));
-//
-//        lineChart.getData().add(series);
-
-
-
-
 }
 
 
